2afe2558ccc719363587b1b4f777a80e5c92bf8f
rayyanalhourani <rayyan.alhourani02@gmail.com>
2024-10-22 10:18:10 +0300

    edit gitignore

--- server/vendor/composer/autoload_real.php
+++ server/vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit3ad2f4f6e6716b8844c56df8dac6528c
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit3ad2f4f6e6716b8844c56df8dac6528c', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit3ad2f4f6e6716b8844c56df8dac6528c', 'loadClassLoader'));
