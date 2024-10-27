8d641e26ac52652a0ded84f8e68f87773fca0523
rayyanalhourani <rayyan.alhourani02@gmail.com>
2024-10-27 10:31:10 +0300

    create service container

---
+++ server/src/services/redis.php
@@ -0,0 +1,68 @@
+<?php
+
+use Predis\Client;
+
+
+class Redis
+{
+    public $redis;
+
+    public function __construct($scheme, $host, $port)
+    {
+        $this->redis = new Client([
+            'scheme' => 'tcp',
+            'host'   => '127.0.0.1',
+            'port'   => 6379,
+        ]);
+
+        if ($this->redis->ping()) {
+            error_log("Connected to Redis server.");
+        } else {
+            error_log("Failed to connect to Redis.");
+        }
+    }
+
+    function storeAllScores($scores, $date)
+    {
+        global $redis;
+
+        foreach ($scores as $leagueName => $matches) {
+            foreach ($matches as $match) {
+                $team1 = $match['teams'][0]['name'];
+                $team2 = $match['teams'][1]['name'];
+
+                $key = "$date:$leagueName:$team1:$team2";
+
+                $redis->hMSet($key, [
+                    'date' => $match['date'],
+                    'stadium' => $match['stadium'],
+                    'city' => $match['city'],
+                    'status' => $match['TimeOrStatus'],
+                    'team1' => json_encode($match['teams'][0]),
+                    'team2' => json_encode($match['teams'][1])
+                ]);
+            }
+        }
+    }
+
+    function findKeysByPattern($pattern)
+    {
+        global $redis;
+        return $redis->keys($pattern);
+    }
+
+    function findByKey($key)
+    {
+        global $redis;
+        return $redis->hGetAll($key);
+    }
+
+    function findAllByKeys($keys)
+    {
+        $results = [];
+        foreach ($keys as $key) {
+            $results[$key] = $this->findByKey($key);
+        }
+        return $results;
+    }
+}
---
+++ server/src/services/scraping.php
@@ -0,0 +1,125 @@
+<?php
+
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\Chrome\ChromeOptions;
+use Facebook\WebDriver\WebDriverBy;
+
+class Scraping
+{
+    public $driver;
+
+    public function __construct($host)
+    {
+        try {
+            $host = 'http://10.123.41.244:4444/';
+            $capabilities = DesiredCapabilities::chrome();
+            $chromeOptions = new ChromeOptions();
+            $chromeOptions->addArguments(['--headless']); // <- comment out for testing
+            $capabilities->setCapability(ChromeOptions::CAPABILITY_W3C, $chromeOptions);
+
+            $this->driver = RemoteWebDriver::create($host, $capabilities);
+            $this->driver->manage()->window()->maximize();
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+
+
+    public function getScores(int $year, int $month, int $day)
+    {
+        global $driver;
+
+        $driver->get("https://www.espn.in/football/scoreboard/_/date/$year$month$day");
+        $MainContainer = $driver->findElement(WebDriverBy::xpath('//*[@role="main"]/div/div'));
+
+        $date = $MainContainer->findElement(WebDriverBy::xpath('./header/div/h3'))->getText();
+        $allLeagues = $MainContainer->findElements(WebDriverBy::xpath('./div'));
+
+        $allScores = [];
+
+        foreach ($allLeagues as $leagues) {
+            $leagueParts = $leagues->findElements(WebDriverBy::xpath('./section'));
+
+            foreach ($leagueParts as $league) {
+                $leagueName = $league->findElement(WebDriverBy::xpath('./header'))->getText();
+                $matches = $league->findElements(WebDriverBy::xpath('./div/section'));
+
+                foreach ($matches as $match) {
+                    $matchDetails = [];
+                    $matchDetails["date"] = $date;
+
+                    $gameDetails = $match->findElements(WebDriverBy::xpath('./div'))[0]
+                        ->findElements(WebDriverBy::xpath('./div/div'));
+
+                    $placeDetails = $gameDetails[1];
+                    $teamsDetails = $gameDetails[0];
+
+                    $this->extractLocationDetails($placeDetails, $matchDetails);
+                    $this->extractTimeOrStates($teamsDetails, $matchDetails);
+                    $this->extractTeamsDetails($teamsDetails, $matchDetails);
+
+                    $allScores[$leagueName][] = $matchDetails;
+                }
+            }
+        }
+
+        return $allScores;
+    }
+
+    private function extractLocationDetails($placeDetails, &$matchDetails)
+    {
+        $divs = $placeDetails->findElements(WebDriverBy::xpath('./div/div/div/div'));
+
+        $stadium = "";
+        $city = "";
+
+        if (sizeof($divs) > 0) {
+            $stadium = $divs[0]->getText();
+            $city = $divs[1]->getText();
+        }
+
+        $matchDetails["stadium"] = $stadium;
+        $matchDetails["city"] =  $city;
+    }
+
+    private function extractTimeOrStates($teamsDetails, &$matchDetails)
+    {
+        $matchTimeOrStatus = $teamsDetails->findElement(WebDriverBy::xpath('./div/div/div'))->getText();
+        $matchDetails["TimeOrStatus"] = $matchTimeOrStatus;
+    }
+
+    private function extractTeamsDetails($teamsDetails, &$matchDetails)
+    {
+        $teams = $teamsDetails->findElements(WebDriverBy::xpath('./div/div/ul/li'));
+
+        foreach ($teams as $team) {
+            $this->extractTeamDetails($team, $matchDetails);
+        }
+    }
+
+    private function extractTeamDetails($team, &$matchDetails)
+    {
+        $name = $team->findElement(WebDriverBy::xpath('./div/div'))->getText();
+
+        $imgs = $team->findElements(WebDriverBy::xpath('./a/img'));
+        $img = null;
+
+        if (sizeof($imgs) > 0) {
+            $img = $imgs[0]->getAttribute("src");
+        }
+
+        $divs = $team->findElements(WebDriverBy::xpath('./div'));
+        $score = "-";
+
+        if (sizeof($divs) == 3) {
+            $score = $divs[2]->getText();
+        }
+
+        $matchDetails["teams"][] = [
+            "name" => $name,
+            "img"  => $img,
+            "score" => $score
+        ];
+    }
+}
