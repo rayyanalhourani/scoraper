80897270fa5c3f0e5500d7527d3debac568a448b
rayyanalhourani <rayyan.alhourani02@gmail.com>
2024-10-28 15:07:17 +0300

    edit cache

--- server/src/socket.php
+++ server/src/socket.php
@@ -37,7 +37,6 @@ $server->on('Open', function (Server $server, Request $request) use ($fds) {
 $server->on('Message', function (Server $server, Frame $frame) use ($fds) {
     $date = $frame->data;
 
-    echo $date;
     if (!preg_match('/^\d{8}$/', $date)) {
         $server->push($frame->fd, "Invalid input.");
         return;
@@ -45,6 +44,88 @@ $server->on('Message', function (Server $server, Frame $frame) use ($fds) {
 
     $fd = $frame->fd;
     $name = $fds->get($frame->fd, "name");
+    connectUser($date, $fd, $name);
+
+    $result = json_encode(getResult($date));
+
+    $server->push($fd, $result);
+});
+
+$server->on('Close', function (Server $server, int $fd) use ($fds) {
+    $fdData = $fds->get($fd);
+    $date = $fdData['date'] ?? null;
+    if ($date !== null) {
+        disconnectUser($date);
+    }
+
+    $fds->del($fd);
+    echo "Connection close: {$fd}, total connections: " . $fds->count() . "\n";
+});
+
+$server->on('Disconnect', function (Server $server, int $fd) use ($fds) {
+    $fdData = $fds->get($fd);
+    $OldDate = $fdData['date'] ?? null;
+    if ($OldDate !== null) {
+        disconnectUser($OldDate);
+    }
+
+    $fds->del($fd);
+    echo "Disconnect: {$fd}, total connections: " . $fds->count() . "\n";
+});
+
+$server->tick(5000, function () use ($fds, $redis,$server) {
+    $keys = $redis->findKeysByPattern("date:*");
+
+    $current_time = new DateTime();
+    $current_date = $current_time->format('Ymd');
+
+    foreach ($keys as $key) {
+        $date = substr($key, 5);
+
+        if ($date >= $current_date && $redis->get("users:$date")>0) {
+            $time_string = $redis->get("time:$date");
+            
+            $event_time = DateTime::createFromFormat('H:i', $time_string);
+
+            $diff = $current_time->diff($event_time);
+            $minutes_diff = ($diff->h * 60) + $diff->i;
+
+            if ($minutes_diff >= 5) {
+                storeInCache($date);
+
+                $fdList=$fds->get("date",$date);
+                $result = $redis->getAllScores($date);
+                foreach($fdList as $fd){
+                    $result = json_encode($result);
+                    $server->push($fd, $result);
+                }
+            }
+        }
+    }
+});      
+
+$server->start();
+
+function getResult($date)
+{
+    global $redis;
+
+    if(!$redis->exists("date:$date")){
+        storeInCache($date);
+    }
+
+    return $redis->getAllScores($date);
+}
+
+function connectUser($date, $fd, $name)
+{
+    global $redis, $fds;
+
+    $fdData = $fds->get($fd);
+    $OldDate = $fdData['date'] ?? null;
+    if ($OldDate != null) {
+        disconnectUser($OldDate);
+    }
 
     $fds->set($fd, [
         'fd' => $fd,
@@ -52,59 +133,23 @@ $server->on('Message', function (Server $server, Frame $frame) use ($fds) {
         'date' => $date
     ]);
 
-    $result=json_encode(getResult($date));
-
-    $server->push($fd, $result);
-});
-
-$server->on('Close', function (Server $server, int $fd) use ($fds) {
-    $date = $fds->get('fd', $fd)['date'];
-    disconnectDate($date);
-
-    $fds->del($fd);
-    echo "Connection close: {$fd}, total connections: " . $fds->count() . "\n";
-});
-
-$server->on('Disconnect', function (Server $server, int $fd) use ($fds) {
-    $date = $fds->get('fd', $fd)['date'];
-    disconnectDate($date);
-
-    $fds->del($fd);
-    echo "Disconnect: {$fd}, total connections: " . $fds->count() . "\n";
-});
-
-$server->start();
-
-function checkTheDate($date)
+    $key = "users:$date";
+    $redis->incr($key);
+}
+function disconnectUser($date)
 {
     global $redis;
-    $key = "date:$date";
-
-    if ($redis->exists($key)) {
-        $redis->incr($key);
-    } else {
-        $redis->set($key, 0);
-    }
-}
-
-function getResult($date)
-{
-    global $redis,$scraping;
-    checkTheDate($date);
-
-    $key = "date:$date";
-
-    if($redis->get($key)==0){
-        $results=$scraping->getScores($date);
-        $redis->storeAllScores($results,$date);
-    }
-
-    return $redis->getAllScores($date);
-}
-
-function disconnectDate($date)
-{
-    global $redis;
-    $key = "date:$date";
+    $key = "users:$date";
     $redis->decr($key);
+}
+
+function storeInCache($date){
+    global $redis, $scraping;
+
+    $current_time = (new DateTime())->format('H:i');
+
+    $results=$scraping->getScores($date);
+    $redis->set("date:$date",1);
+    $redis->set("time:$date",$current_time);
+    $redis->storeAllScores($results,$date);
 }
