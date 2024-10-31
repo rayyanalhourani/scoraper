adb44a3cf6f804ba5f1323a2ff20bb139dfeb963
rayyanalhourani <rayyan.alhourani02@gmail.com>
2024-10-31 10:09:53 +0300

    add title to terminal

--- start.sh
+++ start.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-gnome-terminal -- bash -c "java -jar server/selenium-server-4.25.0.jar standalone; exec bash"
+
+export TITLE="Selenium Server"
+gnome-terminal -- bash -c "echo -ne \"\033]0;${TITLE}\007\"; java -jar server/selenium-server-4.25.0.jar standalone; exec bash"
+
 sleep 15
-gnome-terminal -- bash -c "php server/index.php; exec bash"
-gnome-terminal -- bash -c "php client/yii serve; exec bash"
+
+export TITLE="Server"
+gnome-terminal -- bash -c "echo -ne \"\033]0;${TITLE}\007\"; php server/index.php; exec bash"
+
+export TITLE="Client"
+gnome-terminal -- bash -c "echo -ne \"\033]0;${TITLE}\007\"; php client/yii serve; exec bash"
